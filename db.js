38cf939fe7cea5cd998db83c12ece320b9a3ec34
harrysid786 <haroon@mintedmoney.com>
2026-04-26 18:02:54 +0100

    Use DB_PATH env var for SQLite location (enables Render Disk persistence)

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Database = require("better-sqlite3");
 const path = require("path");
 
-const db = new Database(path.join(__dirname, "quotes.db"));
+const db = new Database(process.env.DB_PATH || path.join(__dirname, "quotes.db"));
 
 // Create tables on startup
 db.exec(`
